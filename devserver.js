5af885437ac65422861c37e2f387f94d0bd13657
Calder Hayes <calderhayes@gmail.com>
2016-12-23 18:26:31 -0500

    Adjusted the webpack devserver to always serve index.html

--- devserver.js
+++ devserver.js
@@ -1,27 +1,23 @@
-/*eslint no-console:0 */
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 var address = 'localhost';
-// var open = require('open');
 
-//new WebpackDevServer(webpack(config), config.devServer)
-//.listen(config.port, 'localhost', function(err) {
-//  if (err) {
-//    console.log(err);
-//  }
-//  console.log('Listening at localhost:' + config.port);
-//  //console.log('Opening your system browser...');
-//  //open('http://localhost:' + config.port);
-//});
 
-new WebpackDevServer(webpack(config), { https: true, hot: true })
-//.listen(config.port, config.devServer.listenHost, function (err) {
-.listen(8080, address, function (err) {
+var server = new WebpackDevServer(webpack(config),
+  {
+    https: true,
+    hot: true
+  });
+
+// Important part. Send down index.html for all requests
+server.use('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+server.listen(8080, address, function (err) {
     if (err) {
         console.log(err);
     }
     console.log('Listening at '+ address);
-    //console.log('Opening your system browser...');
-    //open('http://localhost:' + config.port);
 });
