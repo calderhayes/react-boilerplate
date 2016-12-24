6d4fb7510855a5826649f3028336f31858a02110
Calder Hayes <calderhayes@gmail.com>
2016-12-23 21:35:40 -0500

    Added the lazy loading of react logs

--- src/view/base-component.tsx
+++ src/view/base-component.tsx
@@ -13,7 +13,18 @@ class BaseComponent<P, S> extends React.Component<P, S> {
   public readonly store: IStore;
   public readonly actions: ActionControl;
   public readonly eventEmitter: IEventEmitter;
-  protected readonly log: ILogger;
+
+  protected get log() {
+    if (!this._log) {
+      const prefix = (this as any).constructor.name;
+      this._log = getReactLog(prefix);
+    }
+
+    return this._log;
+  };
+
+  private _log: ILogger;
+
 
   constructor(props: P) {
     super(props);
@@ -21,9 +32,6 @@ class BaseComponent<P, S> extends React.Component<P, S> {
     this.store = DIControl.store;
     this.actions = DIControl.actionControl;
     this.eventEmitter = DIControl.eventEmitter;
-
-    const prefix = (this as any).constructor.name;
-    this.log = getReactLog(prefix);
   }
 
 }
