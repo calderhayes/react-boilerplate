be92ed036e228577e0307cace442a055b55dd8bf
Calder Hayes <calderhayes@gmail.com>
2017-07-28 13:17:48 -0400

    Removed some comments

--- index.tsx
+++ index.tsx
@@ -15,14 +15,9 @@ import {initialize} from './src/util/i18n';
 Log.info('Bootstrapping...');
 Log.info('Bootstrapping under environment: ' + Config.ENVIRONMENT);
 
-// TODO: Split up UI into Smart (stateful components / Containers) and Dumb (functional components / Presentation)
-
-// TODO: Solidify authentication strategy
-// TODO: Solidify what actions are doing vs reducers in terms of error handling
 // TODO: Solidify error message handling, what provides the keys, what creates the keys (this may just be documentation)
 // TODO: Create an automated front end task handler (refresh token), have a centralized
 //   control that handles this in a generalized way
-// TODO: Take a look at tsfmt
 
 
 const API: IAPIService = APIServiceFactory.Create(ApiLog);
