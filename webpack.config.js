49cd05ee1921e8518c3b732eabee8e430c8a1c42
Calder Hayes <calderhayes@gmail.com>
2017-08-13 21:15:23 -0400

    Upgraded webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
 /* tslint:disable no-console */
 var webpack = require('webpack');
-path = require('path');
-//console.log('webpack relative root: ', path.join(__dirname, '/demo/'));
+var path = require('path');
+var {CheckerPlugin, TsConfigPathsPlugin} = require('awesome-typescript-loader');
+var debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
+  target: 'web',
+  context: __dirname,
+  devtool: debug ? 'inline-source-map' : false,
   entry: ["./src/index.ts"],
   output: {
     path: __dirname,
     filename: "./dist/bundle.js"
   },
   module: {
-    preLoaders: [
+    /*rules: [
       {
+        enforce: 'pre',
         test: /\.(tsx|ts)?$/,
-        loader: 'tslint'
+        loader: 'tslint-loader'
       }
-    ],
+    ],*/
     loaders: [
       {
         test: /\.(tsx|ts)(\?.*$|$)/,
-        loader: 'ts-loader'
+        loader: 'awesome-typescript-loader',
+        query: {
+          useWebpackText: true,
+          // tsconfig: ngAppResolve('./src/demo-app/tsconfig.json'),
+          // resolveGlobs: false,
+          module: "es2015",
+          target: "es5",
+          lib: ['es6', 'dom'],
+          useForkChecker: true
+        }
       },
       {
         test: /\.css$/,
@@ -39,14 +54,34 @@ module.exports = {
     ]
   },
   resolve: {
-     extensions: ['', '.js', '.jsx', '.tsx', '.ts'],
-      alias: {
-        styles: path.join(__dirname, '/src/styles/')
-      }
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    alias: {
+      styles: path.join(__dirname, '/src/styles/')
+    },
+    plugins: [
+      new TsConfigPathsPlugin()
+    ]
   },
   plugins: [
+    // new TsConfigPathsPlugin(),
+    new CheckerPlugin(),
     new webpack.DefinePlugin({
         'ENVIRONMENT': JSON.stringify(process.env.NODE_ENV || 'LOCAL_DEV')
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        tslint: {
+          emitErrors: true,
+          failOnHint: true
+        }
+      }
     })
-  ]
+  ],
+  node: {
+    // global: 'window',
+    // crypto: 'empty',
+    module: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
 };
